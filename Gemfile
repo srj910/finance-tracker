f403dc48a78df745f0eac2eb248d251690db15af
Stephen R. Jacobs <stephenr.jacobs@gmail.com>
2021-03-28 20:17:56 -0400

    Added view and other components to lookup stock symbol and display results

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'devise-bootstrap-views', '~> 1.0'
 #IEX Ruby Client gem for tracking stock data
 gem 'iex-ruby-client'
 
+#font-awesome
+gem "font-awesome-rails"
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,10 +2,11 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_03727b5f3fd44f02acac86d6221913c5',
-      secret_token: 'Tsk_58910e18896d4200b8d151ac7796343d',
+      publishable_token: Rails.application.credentials.iex_cloud_client[:sandbox_api_key],
+      secret_token: Rails.application.credentials.iex_cloud_client[:sandbox_secret_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    #client.price(ticker_symbol)
   end
 end
